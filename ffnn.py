ad9744123e1c08c6f94862dea3cc4a1bf667d44b
Matt Jackson <email@mwjackson.net>
2015-03-21 15:56:23 +0000

    changed activation functions

--- ffnn.py
+++ ffnn.py
@@ -4,7 +4,7 @@ from pybrain.tools.shortcuts import buildNetwork
 from pybrain.datasets import ClassificationDataSet
 from pybrain.supervised.trainers import BackpropTrainer
 
-from pybrain.structure import TanhLayer
+from pybrain.structure import TanhLayer, SoftmaxLayer
 
 from pybrain.utilities import percentError
 import random
@@ -35,14 +35,14 @@ print ds.getLinked(0)
 
 # build network
 # by default hidden layer is sigmoid
-net = buildNetwork(ds.indim, 5, ds.outdim, bias=True)  # hiddenclass=TanhLayer)
+net = buildNetwork(ds.indim, 10, ds.outdim, bias=True, hiddenclass=TanhLayer, outclass=SoftmaxLayer)
 test_ds, train_ds = ds.splitWithProportion(0.15)
 
 # create backprop trainer
-trainer = BackpropTrainer(net, train_ds, learningrate=0.3, lrdecay=0.7, momentum=0.0, verbose=True, batchlearning=False, weightdecay=0.1)
+trainer = BackpropTrainer(net, train_ds, learningrate=0.01, lrdecay=1.0, momentum=0.1, verbose=True, batchlearning=False, weightdecay=0.0)
 
 # train until end
-errors = trainer.trainUntilConvergence(maxEpochs=50, verbose=True, continueEpochs=10, validationProportion=0.1)
+errors = trainer.trainUntilConvergence(maxEpochs=1000, verbose=True, continueEpochs=10, validationProportion=0.15)
 
 train_result = trainer.testOnClassData()
 test_result = trainer.testOnClassData(dataset=test_ds)
@@ -51,13 +51,13 @@ print 'total epochs: {0} training error: {1}% test error: {2}%'.format(
     trainer.totalepochs, percentError(train_result, [0, 1]), percentError(test_result, [0, 1]))
 
 predicated_values = test_result
-actual_values = [v[1] for v in list(test_ds['target'])]  # convert categorical back to single var
+actual_values = [int(v[1]) for v in list(test_ds['target'])]  # convert categorical back to single var
 
 print 'actual:'
 print actual_values
 print 'predicted:'
 print predicated_values
-print 'precision: '+str(precision_score(actual_values, predicated_values))
-print 'recall: '+str(recall_score(actual_values, predicated_values))
+print 'precision: {0}'.format(str(precision_score(actual_values, predicated_values)))
+print 'recall: {0}'.format(str(recall_score(actual_values, predicated_values)))
 print 'confusion matrix:'
 print confusion_matrix(actual_values, predicated_values)
